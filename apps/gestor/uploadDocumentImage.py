6498a8198c0c45ae1bead67057cb598787342d50
David Caballero <davidcame124@gmail.com>
2025-07-02 18:36:09 -0500

    Update form digitalizacion - prevent default onsubmit por ajax

--- apps/gestor/uploadDocumentImage.py
+++ apps/gestor/uploadDocumentImage.py
@@ -7,55 +7,52 @@ import base64
 from django.core.files.base import ContentFile
 import uuid
 from django.contrib import messages
+from django.views.decorators.csrf import csrf_exempt
 
+
+@csrf_exempt
 def capture_document_view(request):
     metadata_schemas = MetadataSchema.objects.all()
-    
+
     if request.method == 'POST':
-        form = DocumentAndSchemaForm(request.POST, request.FILES) 
-        
+        form = DocumentAndSchemaForm(request.POST, request.FILES)
+
         if form.is_valid():
-            print("isValid")
-            # Crear documento
             document = form.save(commit=False)
-            
+
             if 'file' in request.FILES:
                 document.file = request.FILES['file']
             
             document.save()
-            print(document)
-            
-            # Procesar metadatos si es necesario
+
             if not form.cleaned_data.get('metadata', False) and form.cleaned_data.get('metadata_schema'):
                 metadata_schema = form.cleaned_data['metadata_schema']
                 metadata_fields = metadata_schema.fields.all()
                 metadata_values = {}
-                
+
                 for field in metadata_fields:
                     field_name = f"metadata_{field.name}"
                     field_value = request.POST.get(field_name, '')
-                    
+
                     if field_value:
-                        if field.field_type == 'checkbox':
-                            metadata_values[field.name] = field_value == 'on'
-                        else:
-                            metadata_values[field.name] = field_value
-                
+                        metadata_values[field.name] = field_value == 'on' if field.field_type == 'checkbox' else field_value
+
                 document.metadata_values = metadata_values
                 document.save()
-            messages.success(request, 'Documento digitalizado')
-            return redirect('capture_document')
+
+            if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+                return JsonResponse({'success': True})
+            else:
+                messages.success(request, 'Documento digitalizado')
+                return redirect('capture_document')
+
         else:
-            print(request.POST)
-            # Imprimir errores del formulario
-            for field, errors in form.errors.items():
-                print(f"Campo '{field}':")
-                for error in errors:
-                    print(f"  - {error}")
-            # También puedes imprimir los datos del POST para depuración
+            if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+                return JsonResponse({'success': False, 'errors': form.errors}, status=400)
+
     else:
         form = DocumentAndSchemaForm()
-    
+
     return render(request, 'gestor/uploadDocumentImage.html', {
         'metadata_schemas': metadata_schemas,
         'form': form,
